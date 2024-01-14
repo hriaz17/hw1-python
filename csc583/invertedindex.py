5a95a9438eff96dbc735ceb2c6b1a3f179d076ce
Haris Riaz <harisriaz1700@gmail.com>
2024-01-13 21:25:25 -0700

    Initial commit

---
+++ csc583/invertedindex.py
@@ -0,0 +1,26 @@
+
+
+class InvertedIndex:
+
+    def __init__(self,doc):
+        pass
+
+    def read_txt_file(self,input_file):
+        #add your code here
+        docs=None
+        return docs
+
+    def q5_1(self,query):
+        # add your code here
+        return ["Doc1", "Doc4"]
+
+    def q5_2(self,query):
+        # add your code heredddd
+        return ["Doc1","Doc2","Doc3","Doc4"]
+
+
+    def q5_3(self,query):
+        # add your code here
+        answer = None
+        return ["Doc1", "Doc2", "Doc4"]
+
